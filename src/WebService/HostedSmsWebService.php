4a1ec68f4c107eba0da4dc9a9dd3d738bcb117d4
Mikołaj Wałachowski <mikolaj.walachowski@dcs.pl>
2023-09-21 14:45:26 +0200

    HSMS-1389 sendMessage refactor, getDeliveryReports

---
+++ src/WebService/HostedSmsWebService.php
@@ -0,0 +1,62 @@
+<?php
+require 'SoapRequestClient.php';
+class HostedSmsWebService
+{
+    private $client;
+    /** 
+    * Create client for API with credentials
+    * 
+    * @param string $userEmail User login in hostedsms.pl
+    * @param string $password User password in hostedsms.pl
+    */ 
+    public function __construct($userEmail, $password)
+    {
+        $this->client = new SoapRequestClient($userEmail, $password); 
+    }
+
+    public function getDeliveryReports($messageIds, $markAsRead = false)
+    {
+        $params = [
+            'MessageIds' => $messageIds,
+            'MarkAsRead' => $markAsRead
+        ];
+        $response = $this->client->sendRequest('GetDeliveryReports', $params);
+        
+        return $response;
+    }
+
+    /** 
+     * Send message using SimpleApi
+     * 
+     * @param string $sender Sender name
+     * @param string $phone Phone number where sms should be sent
+     * @param string $message Message text
+     * @param string $v (optional)
+     * @param string $convertMessageToGSM7 (optional)
+     * 
+     * @return @var messageId if successful request
+     * 
+     * @throws Exception if failed request
+     */
+    public function sendSms($phone, $message, $sender, $transactionId, 
+    $validityPeriod = null, $priority = 0, $flashSms = false, $costCenter = null, $convertMessageToGSM7 = null) 
+    {
+        $params = [
+            'Phone' => $phone,
+            'Message' => $message,
+            'Sender' => $sender,
+            'TransactionId' => $transactionId,
+            'ValidityPeriod' => $validityPeriod,
+            'Priority' => $priority,
+            'FlashSms' => $flashSms,
+            'CostCenter' => $costCenter,
+            'ConvertMessageToGSM7' => $convertMessageToGSM7
+        ];
+
+        $response = $this->client->sendRequest('SendSms', $params);
+
+        return $response; 
+    }
+    
+
+}
