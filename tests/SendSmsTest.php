71649998c0897e7d16fe5a1df6457aee432e3a80
bartosz.seweryn <bartosz.seweryn@dcs.pl>
2023-09-22 15:59:14 +0200

    tests

--- tests/SendSmsTest.php
+++ tests/SendSmsTest.php
@@ -11,31 +11,43 @@ class SendSmsTest extends TestCase
         $this->hostedSms = new HostedSmsSimpleApi();
     }
 
+    public function providerTestData()
+    {
+        return [
+            //['mikolaj.walachowski@dcs.pl', 'HsmsTestPassword4', 'TestowySMS', '48501954841'],
+            [ getenv('HSMS_TEST_USERNAME'), getenv('HSMS_TEST_PASSWORD'), getenv('HSMS_TEST_SENDER'), getenv('HSMS_TEST_PHONE')]
+        ];
+    }
+
     /** @test */
-    // public function test_Send_Sms_Should_Be_Successfull(): void
-    // {
-    //     $this->prepareData();
-    //     $userEmail = 'mikolaj.walachowski@dcs.pl';
-	//     $password = 'HsmsTestPassword4';
-	//     $sender = 'TestowySMS';
-	//     $phone = '48693053151';
-	//     $v = null;
-	//     $convertMessageToGSM7 = false;
-    //     $message = "test sms";
+    /**
+     * @dataProvider providerTestData
+     */
+    public function test_Send_Sms_Should_Be_Successfull($userEmail, $password, $sender, $phone): void
+    {
+        $this->prepareData();
+        // $userEmail = 'mikolaj.walachowski@dcs.pl';
+	    // $password = 'HsmsTestPassword4';
+	    // $sender = 'TestowySMS';
+	    // $phone = '48693053151';
+        $message = "test sms";
+	    $v = null;
+	    $convertMessageToGSM7 = false;
 
-    //     $response = $this->hostedSms->sendSms(
-    //         $userEmail,
-    //         $password,
-    //         $sender,
-    //         $phone,
-    //         $message,
-    //         $v,
-    //         $convertMessageToGSM7
-    //     );
+        $response = $this->hostedSms->sendSms(
+            $userEmail,
+            $password,
+            $sender,
+            $phone,
+            $message,
+            $v,
+            $convertMessageToGSM7
+        );
 
-    //     $this->assertNotEmpty($response);
-    //     $this->assertNotNull($response);
-    // }
+        echo $response;
+        $this->assertNotEmpty($response);
+        $this->assertNotNull($response);
+    }
 
     /** @test */
     public function test_Send_Sms_With_Invalid_User_Should_Throw_Exception(): void
---
+++ tests/ValidDataTest.php
@@ -0,0 +1,112 @@
+<?php
+require 'vendor/autoload.php';
+use HostedSms\WebService\HostedSmsWebService;
+use PHPUnit\Framework\TestCase;
+class ValidDataTest extends TestCase
+{
+    private $hostedSms;
+    private function prepareData()
+    {
+        $this->hostedSms = new HostedSmsWebService(getenv('HSMS_TEST_USERNAME'), getenv('HSMS_TEST_PASSWORD'));
+    }
+
+    /** @test */
+    public function test_SendSms_Should_Be_Valid()
+    {
+        $this->prepareData();
+        $phone = getenv('HSMS_TEST_PHONE');
+	    $message = 'test';
+        $sender = getenv('HSMS_TEST_SENDER');
+        $currentDateTime = date('Y-m-d H:i:s');
+	    $transactionId = $sender . $phone . $message . $currentDateTime; 
+
+        $response = $this->hostedSms->sendSms($phone, $message, $sender, $transactionId);
+
+        $this->assertNotNull($response->currentTime);
+    }
+
+    public function test_SendSmses_Should_Be_Valid()
+    {
+        $this->prepareData();
+        $phones = [getenv('HSMS_TEST_PHONE')];
+	    $message = 'test';
+        $sender = getenv('HSMS_TEST_SENDER');
+        $currentDateTime = date('Y-m-d H:i:s');
+	    $transactionId = $sender . $message . $currentDateTime; 
+
+        $response = $this->hostedSms->sendSmses($phones, $message, $sender, $transactionId);
+
+        $this->assertNotNull($response->currentTime);
+    }
+
+    public function test_CheckPhones_Should_Be_Valid()
+    {
+        $this->prepareData();
+        $phones = [getenv('HSMS_TEST_PHONE')];
+
+        $response = $this->hostedSms->checkPhones($phones);
+
+        $this->assertNotNull($response->currentTime);
+    }
+
+    public function test_ConvertToGsm7_Should_Be_Valid()
+    {
+        $this->prepareData();
+        $text = 'text';
+
+        $response = $this->hostedSms->convertToGsm7($text);
+
+        $this->assertNotNull($response->currentTime);
+    }
+
+    public function test_GetDeliveryReports_Should_Be_Valid()
+    {
+        $this->prepareData();
+        $messageIds = ['750dea2d-0d2e-4d40-ba68-ff39b5164db8', '750dea2d-0d2e-4d40-ba68-ff39b5164db8'];
+
+        $response = $this->hostedSms->getDeliveryReports($messageIds);
+
+        $this->assertNotNull($response->currentTime);
+    }
+
+    public function test_GetInputSmses_Should_Be_Valid()
+    {
+        $this->prepareData();
+        $from = null;
+        $to = null;
+        $recipient = 'test';
+        $markAsRead = false;
+
+        $response = $this->hostedSms->getInputSmses($from, $to, $recipient, $markAsRead);
+
+        $this->assertNotNull($response->currentTime);
+    }
+
+    public function test_GetUnreadDeliveryReports_Should_Be_Valid()
+    {
+        $this->prepareData();
+
+        $response = $this->hostedSms->getUnreadDeliveryReports();
+
+        $this->assertNotNull($response->currentTime);
+    }
+
+    public function test_GetUnreadInputSmses_Should_Be_Valid()
+    {
+        $this->prepareData();
+
+        $response = $this->hostedSms->getUnreadInputSmses();
+
+        $this->assertNotNull($response->currentTime);
+    }
+
+    public function test_GetValidSenders_Should_Be_Valid()
+    {
+        $this->prepareData();
+
+        $response = $this->hostedSms->getValidSenders();
+
+        $this->assertNotNull($response->currentTime);
+    }
+}
+?>
