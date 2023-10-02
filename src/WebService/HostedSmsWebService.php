3ee091a8c7e3e2fb7df3291f824ce65b741132a3
Mikołaj Wałachowski <mikolaj.walachowski@dcs.pl>
2023-10-02 13:19:48 +0200

    HSMS-1389 Usunięcie komentarzy

--- src/WebService/HostedSmsWebService.php
+++ src/WebService/HostedSmsWebService.php
@@ -9,10 +9,6 @@ use DateTimeInterface;
 use HostedSms\WebService\Responses\{CheckPhonesResponse, ConvertToGsm7Response, GetDeliveryReportsResponse, 
     GetInputSmsesResponse, GetUnreadDeliveryReportsResponse, GetUnreadInputSmsesResponse, GetValidSendersResponse, SendSmsResponse, SendSmsesResponse};
 
-//require_once 'Exceptions.php';
-//require_once 'Responses.php';
-//require_once 'SoapRequestClient.php';
-
 class HostedSmsWebService
 {
     private $client;
